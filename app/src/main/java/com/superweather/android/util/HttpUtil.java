55fd92a5e21932dec071d9b56161897a402e4fe8
pan <pan452124@gmail.com>
2017-05-23 03:26:45 +0800

    成省市县数据查看. git commit -m 成省市县数据查看。

---
+++ app/src/main/java/com/superweather/android/util/HttpUtil.java
@@ -0,0 +1,19 @@
+package com.superweather.android.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Administrator on 2017/5/22.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
